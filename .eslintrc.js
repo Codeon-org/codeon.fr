110d3707e28c0736ab433449138d4291659539d6
MrAnyx <1ptitigre@gmail.com>
2021-10-04 12:05:44 -0400

    Fix: Added eslint and prettier + sass loader

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
+  },
+  extends: ["@nuxtjs", "plugin:nuxt/recommended", "prettier"],
+  plugins: ["prettier"],
+  // add your custom rules here
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
