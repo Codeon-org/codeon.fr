decc939c99974c2fe3cb324a8b21a98b571b4d62
MrAnyx <1ptitigre@gmail.com>
2022-02-26 23:18:36 +0100

    💬 Added education section

---
+++ data/education.js
@@ -0,0 +1,55 @@
+const educations = [
+  {
+    school: "3iL Ingénieurs",
+    fields: ["Computer Science"],
+    city: "Limoges",
+    country: "France",
+    from: "September 2017",
+    to: "July 2022",
+    diplomas: [
+      {
+        date: "September 2022",
+        name: "Engineering Degree",
+        info: "with high honors",
+      },
+      {
+        date: "June 2021",
+        name: "TOEIC",
+        info: "with a score of 920/990",
+      },
+    ],
+  },
+  {
+    school: "Université du Québec à Chicoutimi (UQAC)",
+    fields: ["Computer Science", "Maths"],
+    city: "Chicoutimi",
+    country: "Québec",
+    from: "September 2021",
+    to: "December 2021",
+  },
+  {
+    school: "Integrated Preparatory Class (3iL)",
+    fields: ["Computer Science", "Maths"],
+    city: "Limoges",
+    country: "France",
+    from: "September 2017",
+    to: "May 2019",
+  },
+  {
+    school: "Lycée Saint Pierre Saint Paul",
+    fields: ["General", "Science"],
+    city: "Dreux",
+    country: "France",
+    from: "September 2014",
+    to: "July 2017",
+    diplomas: [
+      {
+        date: "July 2017",
+        name: "Scientific Bachelor Degree",
+        info: "with high honours",
+      },
+    ],
+  },
+];
+
+export default educations;
